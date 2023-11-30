fe7de7c46dbdbb4b6160393b819c3031caa587ac
iamzehan <zehan.kareem@gmail.com>
2023-11-30 16:28:34 +0600

    Tidying up

--- object_detection_project.bat
+++ object_detection_project.bat
@@ -5,6 +5,8 @@ if "%1" == "" (
     echo Usage: %0 ^<project_name^>
     exit /b 1
 )
+@REM # Crete a Projects Folder to create all the projects in this folder.
+cd Projects
 
 set "PROJECT_NAME=%1"
 set "ROOT_DIR=%CD%\%PROJECT_NAME%"
